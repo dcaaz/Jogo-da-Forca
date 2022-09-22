5aecf96b0c3fdb11be00029b3c1dcba1d32f6bf4
dcaaz <danicreaazevedo@gmail.com>
2022-09-22 13:08:46 -0300

    layout e map letras

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 export default function App() {
     const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+    // JSX - JavaScript and XML
     return (
         <div>
             <div class="forca">
