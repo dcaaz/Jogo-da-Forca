add2ba44749ed76c970f4a58e9381281faebafb6
dcaaz <danicreaazevedo@gmail.com>
2022-09-25 18:17:00 -0300

    input de chute e fim de jogo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React, { useCallback } from "react";
+import React from "react";
 import "./styles/reset.css"
 import "./styles/style.css";
-import palavras from "./palavras";
 import forca0 from "./imagens/forca0.png"
 import forca1 from "./imagens/forca1.png"
 import forca2 from "./imagens/forca2.png"
@@ -13,13 +12,14 @@ import getPalavras from "./palavras";
 
 export default function App() {
     const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    const [corTecla, setCorTecla] = React.useState("cinza");
     const [contador, setContador] = React.useState(0);
     const [palavraSorteada, setPalavraSorteada] = React.useState('');
     const [pSorteadaNormalizada, setPSorteadaNormalizada] = React.useState([]);
-    const [palavraUnderlines, setPUnderlines] = React.useState([]);
+    const [pUnderlines, setPUnderlines] = React.useState([]);
     const [pFinalizada, setPFinalizada] = React.useState("");
-    const [lClicada, setLClicada] = React.useState([])
+    const [lClicada, setLClicada] = React.useState([]);
+    const [imput, setImput] = React.useState("");
+    // const [disable, setDisable] = React.useState(false);
 
     const imagens = [forca0, forca1, forca2, forca3, forca4, forca5, forca6];
 
@@ -38,7 +38,6 @@ export default function App() {
 
     function iniciarPartida() {
         sortearPalavra();
-        setCorTecla("azul");
     }
 
     function letraClicada(letraClick, index) {
@@ -47,7 +46,7 @@ export default function App() {
         setLClicada(clicado);
 
         if (pSorteadaNormalizada.includes(letraClick)) {
-            let newPalavraUnderlines = [...palavraUnderlines];
+            let newPalavraUnderlines = [...pUnderlines];
             let indicesLetraNaPalavra = pSorteadaNormalizada.map((letraPalavra, idx) => letraPalavra === letraClick ? idx : -1).filter((idx) => idx !== -1);
             indicesLetraNaPalavra.forEach((index) => newPalavraUnderlines[index] = palavraSorteada[index]);
 
@@ -65,20 +64,35 @@ export default function App() {
 
         if (contador === 6) {
             setPFinalizada("vermelho")
-        } else {
-            setPFinalizada("verde")
+            jogoFinalizado()
         }
     }
 
+    function botaoImput() {
+        if (imput === palavraSorteada) {
+            setPUnderlines(imput);
+            setPFinalizada("verde");
+            jogoFinalizado();
+        } else {
+            setPUnderlines(imput);
+            setPFinalizada("vermelho");
+            setContador(6);
+            jogoFinalizado();
+        }
+    }
 
-    // JSX - JavaScript and XML 
+    function jogoFinalizado(){
+        //desabilitar teclado
+        //desabilitar imput
+    }
+
     return (
         <>
             <div className="forca">
                 <img src={imagens[contador]} alt="forca" />
                 <div className="forca-itens">
                     <button onClick={iniciarPartida}>Escolher Palavra</button>
-                    <h1 className={pFinalizada}>{palavraUnderlines}</h1>
+                    <h1 className={pFinalizada}>{pUnderlines}</h1>
                 </div>
             </div>
             <div className="teclado-virtual">
@@ -97,8 +111,18 @@ export default function App() {
             </div>
             <div className="adivinhar-palavra">
                 <h1>Já sei a palavra!</h1>
-                <input></input>
-                <button className={corTecla}>Chutar</button>
+
+                <input
+                    placeholder="Digete  seu chute..."
+                    onChange={(event) => setImput(event.target.value)}
+                    value={imput}
+                />
+
+                <button
+                    onClick={botaoImput}>
+                    Chutar
+                </button>
+
             </div>
         </>
     )
